e07b8af2b455d2527b34bf28a54da285e24a7239
Jekko07 <jranara.webdev@gmail.com>
2025-01-21 14:41:07 +0800

    initialized packages for wallet and transaction package

--- packages/wallets/package.js
+++ packages/wallets/package.js
@@ -1,24 +1,16 @@
 Package.describe({
-  name: 'wallets',
-  version: '0.0.1',
+  name: "wallets",
+  version: "0.0.1",
   // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  summary: ""
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('2.7');
-  api.use('ecmascript');
-  api.mainModule('wallets.js');
-});
+Package.onUse(function (api) {
+  api.versionsFrom("2.7");
+  api.use("ecmascript");
+  // api.mainModule('wallets.js');
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('wallets');
-  api.mainModule('wallets-tests.js');
+  api.addFiles(["lib/collections/WalletsCollection.js"], ["client", "server"]);
+
+  api.export("WalletsCollection", ["client", "server"]);
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { ContactsCollection } from "meteor/contacts/lib/collections/ContactsCollection";
+import { WalletsCollection } from "meteor/wallets/lib/collections/WalletsCollection";
+import { TransactionsCollections } from "meteor/transactions/lib/collections/TransactionsCollections";
 import "meteor/contacts/server/ContactsMethods";
 import "meteor/contacts/server/ContactsPublications";
 
