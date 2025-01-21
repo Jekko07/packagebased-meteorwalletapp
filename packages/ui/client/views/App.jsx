e6d8284f1bee43ae6a83f257fab4f1b7b9dafab5
Jekko07 <jranara.webdev@gmail.com>
2025-01-21 14:59:35 +0800

    added wallet.jsx UI

--- packages/ui/client/views/App.jsx
+++ packages/ui/client/views/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { ContactForm } from "meteor/contacts/client/ContactForm";
 import { ContactList } from "meteor/contacts/client/ContactList";
 import { Header } from "./Header";
+import { Wallet } from "meteor/wallets/client/Wallet";
 
 export const App = () => (
   <div>
     <Header />
     <div className="min-h-full">
       <div className="max-w-4xl mx-auto p-2">
+        <Wallet />
         <ContactForm />
         <ContactList />
       </div>
